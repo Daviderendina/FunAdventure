fbb4e91bb9cfe40bb4ead063374e866af0b65eeb
root <root@localhost.localdomain>
2021-01-05 20:09:29 +0100

    Changed utils package

---
+++ src/main/java/com/rendinadavide/assignment3/utils/IdGenerator.java
@@ -0,0 +1,20 @@
+package com.rendinadavide.assignment3.utils;
+
+import java.util.UUID;
+
+public class IdGenerator {
+
+    private static IdGenerator istance;
+
+    private IdGenerator(){}
+
+    public synchronized static IdGenerator getIstance() {
+        if(istance == null)
+            istance = new IdGenerator();
+        return istance;
+    }
+
+    public String getUID(){
+        return UUID.randomUUID().toString();
+    }
+}
